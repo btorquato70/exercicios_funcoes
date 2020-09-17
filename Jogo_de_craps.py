d8548504b4290b9ae96c066441277b15509bfadf
Bruna Cristina <btorquato70@gmail.com>
2020-09-17 20:22:49 -0300

    Abrir arquivo para exercicio jogo de craps

---
+++ Jogo_de_craps.py
@@ -0,0 +1,7 @@
+#Faça um programa de implemente um jogo de Craps. O jogador lança um par de dados,
+#obtendo um valor entre 2 e 12. Se, na primeira jogada, você tirar 7 ou 11, você um "natural" e ganhou.
+#Se você tirar 2, 3 ou 12 na primeira jogada, isto é chamado de "craps" e você perdeu devendo R$ 10.000 para a Nat
+#e para o Groger. Se, na primeira jogada, você fez um 4, 5, 6, 8, 9 ou 10, você perdeu não devendo nada para nós.
+
+
+
